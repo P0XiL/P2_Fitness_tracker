9e79d0ae4347c783f2728ba55af91e2aa9abeea7
Tobias Lauritsen <tvla23@student.du>
2024-04-03 15:45:28 +0200

    Working on Graph with friends

--- Graph.js
+++ Graph.js
@@ -65,9 +65,93 @@ assholeblaster69:{
       },
 
     }
-}
+
+},
+assholeblaster63:{
+  daily:{
+   "31/3/2024": {
+      "type": "pushups",
+      "target": 100,
+      "amount": 5
+    },
+
+    "24/2/2024": {
+      "type": "run",
+      "target": 100,
+      "amount": 5
+    },
+
+    "30/2/2024": {
+      "type": "pushups",
+      "target": 100,
+      "amount": 5
+    },
+
+    "21/3/2024": {
+      "type": "run",
+      "target": 10,
+      "amount": 10
+    }
+  },
+
+  weekly:{
+     "25/3/2024":{
+        "type": "cycling",
+        "target": 40,
+        "amount": 40,
+    },
+
+     "22/3/2024":{
+        "type": "walk",
+        "target": 60,
+        "amount": 35,
+    }
+  },
+
+  monthly:{
+    "1/2/2024":{
+        "type": "crunches",
+        "target": 300,
+        "amount": 301,
+    },
+
+  }
+
 }
 
+}
+
+
+
+function userArrayMaker(obj){
+  let userarray = [];
+  for(let users in obj){
+    userarray.push([users]);
+  }
+  console.log(userarray[0]);
+}
+
+
+function PlotWithFriends(type, userarray){
+  let x = [];
+  let i = 0;
+  do{
+    console.log("test2");
+    for (let key in userarray) {
+      console.log("test1");
+      if(questlog.userarray[i].daily[key].type === type){
+        x.push(key);
+      }
+    }
+    i++;
+  } while(i <= userarray.length);
+
+  return x;
+  }
+
+
+console.log(PlotWithFriends("run", userArrayMaker(questlog)));
+  
 
 
 function Plot(user, type) {
@@ -89,7 +173,9 @@ for (let date in user.daily) {
         amountsWithType.push(user.daily[date].amount);
     }
 }
-console.log(amountsWithType); 
+
+
+// ploting with data.
 
 let ctx = document.getElementById("myChart");
 
@@ -117,6 +203,8 @@ let ctx = document.getElementById("myChart");
 }
 
 Plot(questlog.assholeblaster69, "run");
+
+
 
 
 
