4a5cf2fb562bf5ecb9702d04094842616b0b73f3
P0XiL <121467273+P0XiL@users.noreply.github.com>
2024-04-08 10:56:35 +0200

    before rebase

--- Survey.js
+++ Survey.js
@@ -1,19 +1,23 @@
-
-
-
-const form = document.getElementById('surveyForm');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-        if (data[key]) {
-            if (!Array.isArray(data[key])) {
-                data[key] = [data[key]];
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('surveyForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const data = {};
+        for (const [key, value] of formData.entries()) {
+            if (data[key]) {
+                if (!Array.isArray(data[key])) {
+                    data[key] = [data[key]];
+                }
+                data[key].push(value);
+            } else {
+                data[key] = value;
             }
-            data[key].push(value);
-        } else {
-            data[key] = value;
         }
-    }
-    console.log(data);
+        
+        const jsonData = JSON.stringify({ survey: data });
+        console.log(jsonData);
+    });
+});
+
+
