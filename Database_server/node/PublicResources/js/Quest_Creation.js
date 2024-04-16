42e2265c91a0a4736f088184c756e851e29be830
Jacob <jacob74447@gmail.com>
2024-04-16 10:37:44 +0200

    d

--- Database_server/node/PublicResources/js/Quest_Creation.js
+++ Database_server/node/PublicResources/js/Quest_Creation.js
@@ -1,125 +1,164 @@
 
 //Returns an object with relavent user info
-function get_user_info() {
+function get_user_info(){
     //get user info from database
     return userinfo;
 }
-//example of output
-userInfo = {
-    rank: generate_random_number(43),
-    mastery: generate_random_number(3),
-    preset: ["cardio", "cardio", "cardio", "core", "core", "upperbody", "lowerbody"],
+ //example of output
+ userinfo = {
+    rank: generate_random_number(43)+3,
+    mastery: generate_random_number(3)+3,
+    preset:  ["cardio", "cardio", "cardio", "core", "core", "upperbody", "lowerbody"],
 };
 
 //Generate a random number from 0 to max
-function generate_random_number(max) {
+function generate_random_number(max){
     return Math.floor(Math.random() * max);
 }
 
 //Returns a type for the quest based on preset
-function choose_quest_type(preset) {
+function choose_quest_type(preset){    
     return preset[generate_random_number(preset.length)];
 }
 
 
-function choose_quest(quests) {
+//Gets the object with quest for a given type
+function get_quest_object(type){
+    //Connect to database
+    //Path to JSON file
+    fetch('your_file.json')
+        .then(response => {
+        // Check if the request was successful
+        if (!response.ok) {
+            throw new Error('Failed to fetch JSON');
+        }
+        // Parse the JSON response
+        return response.json();
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching or parsing JSON:', error);
+        });
+}
+
+
+function choose_quest(quests){
+    //TODO: TEMP FIX, NEED GET_QUEST_OBJECT TO WORK FISRT
     return quests[Object.keys(quests)[generate_random_number(Object.keys(quests).length)]]
 }
 
-function is_empty_object(obj) {
-    return Object.keys(obj).length === 0;
-}
 
-
-function modify_quest(quest, rank, difficulty, mastery, timespan) {
+function modify_quest(quest, rank, difficulty, mastery){
     //formular for scaling
-    const min = quest["base_target"];
-    switch (timespan) {
-        case 'daily':
-            quest["base_target"] = Math.floor(quest["base_target"] * ((rank + parseInt(difficulty)) * 0.1) * (mastery * 0.5) * 1);
-            break;
-        case 'weekly':
-            quest["base_target"] = Math.floor(quest["base_target"] * ((rank + parseInt(difficulty)) * 0.1) * (mastery * 0.5) * 4);
-            break;
-        case 'monthly':
-            quest["base_target"] = Math.floor(quest["base_target"] * ((rank + parseInt(difficulty)) * 0.1) * (mastery * 0.5) * 13);
-            break;
-    }
-    
-    if (quest["base_target"] < min){
-        quest["base_target"] = min
-        quest["quest_text"] = quest["quest_text"].replace("x", quest["base_target"])
-    }
+    console.log(quest);
+    quest["base_target"] = quest["base_target"] * ((rank + difficulty) * 0.1) * (mastery * 0.5);
     quest["quest_text"] = quest["quest_text"].replace("x", quest["base_target"])
     return quest;
 }
 
 
-async function fetchJSON(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Failed to fetch JSON');
+
+
+quest_log = {
+    assholeblaster69:{
+       daily:{
+        "31/3/2024": {
+           "type": "pushups",
+           "target": 100,
+           "amount": 5
+         },
+ 
+         "26/3/2024": {
+           "type": "run",
+           "target": 10,
+           "amount": 10
+         }
+       },
+ 
+       weekly:{
+          "02/4/2024":{
+             "type": "cycling",
+             "target": 40,
+             "amount": 30,
+         },
+ 
+          "22/3/2024":{
+             "type": "walk",
+             "target": 60,
+             "amount": 35,
+         }
+       },
+ 
+       monthly:{
+         "1/4/2024":{
+             "type": "crunches",
+             "target": 300,
+             "amount": 301,
+         },
+ 
+       }
+    
+     }
+ }
+
+
+function firstOwnKey(object){
+    let firstKey
+    for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+            firstKey = key;
+            break;
         }
-        return await response.json();
-    } catch (error) {
-        console.error('Error fetching JSON', error);
-        return null;
     }
+    return firstKey
 }
 
 
-function check_current(timespan, quest_log, userID) {
-    const obj_currentDate = new Date();
+function check_current(timespan, userID){
+    //connect database
+    current_date = new Date();
     let lastestDate;
-    switch (timespan) {
-        case 'daily': {
-            const obj_dailies = quest_log[userID][timespan];
-            if (is_empty_object(obj_dailies)){
-                return "None"
-            }
-            lastestDate = Object.keys(obj_dailies)[Object.keys(obj_dailies).length - 1];
-            const questDate = lastestDate.split("/");
-            if (questDate[0] == obj_currentDate.getDate() && questDate[1] == (obj_currentDate.getMonth() + 1) && questDate[2] == obj_currentDate.getFullYear()) {
-                if (obj_dailies[lastestDate]["target"] <= obj_dailies[lastestDate]["amount"]) {
+    switch(timespan){
+        case 'daily':{
+            let dailies = quest_log[userID][timespan];
+            lastestDate = firstOwnKey(dailies);
+            let questDate = lastestDate.split("/");
+            if (questDate[0] == current_date.getDate() && questDate[1] == (current_date.getMonth() + 1) && questDate[2] == current_date.getFullYear() ){
+                if(dailies[lastestDate]["target"] <= dailies[lastestDate]["amount"]){
                     return "Done";
                 }
                 return lastestDate;
             }
-        }
+            }
             return "None";
 
         case 'weekly':
-            const obj_weeklies = quest_log[userID][timespan];
-            if (is_empty_object(obj_weeklies)){
-                return "None"
-            }
-            lastestDate = Object.keys(obj_weeklies)[Object.keys(obj_weeklies).length - 1];
-            const questDateStr = lastestDate.split("/");
+            let weeklies = quest_log[userID][timespan];
+            lastestDate = firstOwnKey(weeklies);
+            let questDateStr = lastestDate.split("/");
             //Check if questDay is within 7 days of today
             let questDateObj = new Date(questDateStr[2], questDateStr[1] - 1, questDateStr[0]);
             //Difference in milisec
-            diffInMilisec = obj_currentDate - questDateObj;
+            diffInMilisec = current_date - questDateObj;
             //Convert to day
             let diffInDays = diffInMilisec / (1000 * 60 * 60 * 24)
 
 
-            if (Math.abs(diffInDays) <= 7) {
-                if (obj_weeklies[lastestDate]["target"] <= obj_weeklies[lastestDate]["amount"]) {
+            if (Math.abs(diffInDays) <=7){
+                if(weeklies[lastestDate]["target"] <= weeklies[lastestDate]["amount"]){
                     return "Done";
                 }
                 return lastestDate;
             }
             return "None"
         case 'monthly':
-            const obj_monthlies = quest_log[userID][timespan];
-            if (is_empty_object(obj_monthlies)){
-                return "None"
-            }
-            lastestDate = Object.keys(obj_monthlies)[Object.keys(obj_monthlies).length - 1];
-            const questDate = lastestDate.split("/");
-            if ((obj_currentDate.getMonth() + 1) == questDate[1] && obj_currentDate.getFullYear() == questDate[2]) {
-                if (obj_monthlies[lastestDate]["target"] <= obj_monthlies[lastestDate]["amount"]) {
+            let monthlies = quest_log[userID][timespan];
+            lastestDate = firstOwnKey(monthlies);
+            let questDate = lastestDate.split("/");
+            if ((current_date.getMonth() + 1) == questDate[1] && current_date.getFullYear() == questDate[2]){
+                if(monthlies[lastestDate]["target"] <= monthlies[lastestDate]["amount"]){
                     return "Done";
                 }
                 return lastestDate;
@@ -128,116 +167,77 @@ function check_current(timespan, quest_log, userID) {
         default:
             return "Fail"
 
-
     }
-
-
 }
 
 
 
-function add_quest_json(quest) {
-    fetch('http://127.0.0.1:3364/write_quest_json', { //Change this to either https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node or http://127.0.0.1:3366
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(quest)
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch POST');
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('Error fetch Post quest_log:', error);
+function display_quest(quest, quest_log, userinfox, user){
+    let timespans = ["daily", "weekly", "monthly"];
+    let quest_timespan = timespans[quest[5] - 1];
+    let state = check_current(quest_timespan, user);
+    //let questTemp = get_quest_object();
+    
+    if (state == "None"){
+        const type = choose_quest_type(userinfo["preset"]);
+        document.getElementById(quest + "_type").innerText = "Type: " +  type;
+        //TODO: Should save this somewhere such the user can't just reload the site for new type :hmm:
+
+        //Create button
+        let button = document.createElement("button");
+        button.textContent = "Get new Quest!";
+
+        //Append button
+        document.getElementById(quest + "_type").appendChild(button)
+
+        //Add event listner to button
+        button.addEventListener("click", ()=>{
+            document.getElementById("myModal").style.display = "block";
         });
-}
+        document.getElementById("popupText").innerText = "Choose difficulty for " + quest_timespan + " of type: " + type;
+
+        document.querySelectorAll(".difficulty-button").forEach(button => {
+            button.addEventListener("click", (event) => {
+                const difficulty = event.target.dataset.difficulty;
+                
+                const obj_questOfType = get_quest_object(type);
+                let obj_quest = choose_quest("quests");
 
 
+                //TODO: USE USERINFO TO GET RANK AND MASTERY
+                obj_quest = modify_quest(quest_Obj, 3, difficulty, 6);
+                console.log(quest_Obj.quest_text);
+                document.getElementById(quest + "_type").innerText = "Type: " +  type + "\nQuest: " + quest_Obj.quest_text;
 
-function open_modal_for_quest(quest, questTimespan, type, user) {
-    document.getElementById("myModal").style.display = "block";
-    document.getElementById("popupText").innerText = "Choose difficulty for " + questTimespan + " of type: " + type;
-
-    document.querySelectorAll(".difficulty-button").forEach(difficultyButton => {
-        difficultyButton.addEventListener("click", (event) => {
-            const difficulty = event.target.dataset.difficulty;
-            fetchJSON('json/quest_templates.json')
-                .then(data => {
-                    let obj_Quest = choose_quest(data.quest_templates[type]);
-                    obj_Quest = modify_quest(obj_Quest, 3, difficulty, 6, questTimespan);
-                    obj_newQuest = new Object;
-                    obj_currentDate = new Date;
-                    const date = obj_currentDate.getDate() + '/' + (obj_currentDate.getMonth() + 1) + '/' + obj_currentDate.getFullYear();
-
-                    obj_newQuest[date] = {};
-                    obj_newQuest[date].type = type;
-                    obj_newQuest[date].target = obj_Quest["base_target"];
-                    obj_newQuest[date].amount = 0;
-                    obj_newQuest[date].text = obj_Quest.quest_text;
-                    obj_newQuest[date].state = "incomplete";
-                    obj_newQuest.timespan = questTimespan;
-
-                    add_quest_json(obj_newQuest);
-                    document.getElementById("myModal").style.display = "none";
-                    location.reload();
-                });
-
-        });
-    })
-
-}
-
-function display_quest(quest, userInfox, user) {
-    const timespans = ["daily", "weekly", "monthly"];
-    let questTimespan = timespans[quest[5] - 1];
-    fetchJSON('json/quest_log.json')
-        .then(quest_log => {
-            const stateQuest = check_current(questTimespan, quest_log, user);
-            if (stateQuest == "None") {
-                const type = choose_quest_type(userInfo["preset"]);
-                document.getElementById(quest + "_type").innerText = "Type: " + type;
-                //TODO: Should save this somewhere such the user can't just reload the site for new type :hmm:
-
-                //Create button
-                let button = document.createElement("button");
-                button.textContent = "Get new Quest!";
-                button.id = questTimespan;
-
-                //Append button
-                document.getElementById(quest + "_type").appendChild(button)
-
-                //Add event listner to button
-
-                button.addEventListener("click", () => {
-                    const obj_newQuest = open_modal_for_quest(quest, questTimespan, type, user);
-                });
-
-                document.getElementById("close").addEventListener("click", () => {
-                    document.getElementById("myModal").style.display = "none";
-
-                });
-
-
-            } else if (stateQuest == "Done") {
-                document.getElementById(quest + "_type").innerText = "Quest done";
-                //Add progressbars
-            } else {
-                const vaules = quest_log[user][questTimespan][stateQuest];
-                document.getElementById(quest + "_type").innerText = "Type: " + vaules.type + "\n" + vaules.text + "\nYou have done " + vaules.amount + " out of " + vaules.target;
-            }
-
-            //Add progressbar
-
+                document.getElementById("myModal").style.display = "none";
+            })
         })
 
+        document.getElementById("close").addEventListener("click", () => {
+            document.getElementById("myModal").style.display = "none";
+        });
+        
+
+        
+         // create new quest then 
+    } else if (state == "Done" ){
+        document.getElementById(quest + "_type").innerText = "Quest done"; 
+        //Add progressbars
+    } else {
+        let type = quest_log[user][quest_timespan][state].type;
+        document.getElementById(quest + "_type").innerText = "Type: " + type;
+        console.log(type);
+        
+        //document.getElementById(quest + "_text").innerText = questTemp["cardio"][type].text;
+    }
+         
+        //Add progressbar
 }
 
 
 
 
-display_quest("quest1", "Add User Json Here", "assholeblaster69");
-display_quest("quest2", "Add User Json Here", "assholeblaster69");
-display_quest("quest3", "Add User Json Here", "assholeblaster69");
+display_quest("quest1", quest_log, "Add User Json Here", "assholeblaster69");
+display_quest("quest2", quest_log, "Add User Json Here", "assholeblaster69");
+display_quest("quest3", quest_log, "Add User Json Here", "assholeblaster69");
+console.log(get_quest_object("cardio"));
