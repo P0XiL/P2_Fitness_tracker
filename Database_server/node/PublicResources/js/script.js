87e9531e7a9bb8e0445a021257efd5e9a5362327
TobiasRDL <tlasso23@student.aau.dk>
2024-04-18 10:26:58 +0200

    Update script.js

--- Database_server/node/PublicResources/js/script.js
+++ Database_server/node/PublicResources/js/script.js
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     
     function loginUser(loginData) {
-        fetch('http://127.0.0.1:3360/login', {
+        fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node1/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -166,7 +166,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to send data to server-side script
     function createUser(userData) {
-        fetch('http://127.0.0.1:3360/createUser', { // Change this to either https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node4/writeUserData, or http://127.0.0.1:3364/writeUserData depending on localhost or server host
+        fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node1/createUser', { // Change this to either https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node4/writeUserData, or http://127.0.0.1:3364/writeUserData depending on localhost or server host
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -241,7 +241,7 @@ function clearLoginErrorMessage() {
 
 function fetchUserData(username) {
     // Fetch the JSON data
-    fetch('http://127.0.0.1:3360/json/users_info.json')
+    fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node1/json/users_info.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch userinfo.json: ${response.statusText}`);
@@ -487,7 +487,7 @@ function updatePreset(username, preset) {
 
 
 function update_users_info(newUserInfo) {
-    fetch('http://127.0.0.1:3360/write_user_info_json', {
+    fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node1/write_user_info_json', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
