b79b14e082c719d3a084306e3187d68a773c4288
PJ46LH <pj46lh@student.aau.dk@p2datsw-f24-01.srv.aau.dk>
2024-04-18 08:56:21 +0200

    updated

--- Database_server/node/PublicResources/js/script.js
+++ Database_server/node/PublicResources/js/script.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     
     function loginUser(loginData) {
-        fetch('http://127.0.0.1:3360/login', {
+        fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to send data to server-side script
     function createUser(userData) {
-        fetch('http://127.0.0.1:3360/createUser', { // Change this to either https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node4/writeUserData, or http://127.0.0.1:3364/writeUserData depending on localhost or server host
+        fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/createUser', { // Change this to either https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node4/writeUserData, or http://127.0.0.1:3364/writeUserData depending on localhost or server host
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -207,7 +207,7 @@ function clearLoginErrorMessage() {
 
 function fetchUserData(username) {
     // Fetch the JSON data
-    fetch('http://127.0.0.3360/users_info.json')
+    fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/json/users_info.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch userinfo.json: ${response.statusText}`);
@@ -404,7 +404,7 @@ function updatePreset(username, preset) {
 
 
 function update_users_info(newUserInfo) {
-    fetch('http://127.0.0.1:3360/write_user_info_json', {
+    fetch('https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/write_user_info_json', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
