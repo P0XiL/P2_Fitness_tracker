f5a7dfc98dabf31dba09dc2adb197d2b6313ab63
PJ46LH <pj46lh@student.aau.dk@p2datsw-f24-01.srv.aau.dk>
2024-05-02 13:20:20 +0200

    updated

--- Database_server/node/PublicResources/js/script.js
+++ Database_server/node/PublicResources/js/script.js
@@ -1,4 +1,4 @@
-const serverPath = 'https://127.0.0.1:3360/';
+const serverPath = 'https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/';
 // LOCALHOST: https://127.0.0.1:3360
 // SERVER: https://cs-24-sw-2-06.p2datsw.cs.aau.dk/node9/
 
