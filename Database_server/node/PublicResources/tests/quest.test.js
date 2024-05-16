f31ec83fc04f13dbd6c6160a521c78888f15e848
MikkelT <hp11kx@student.aau.dk>
2024-05-16 09:44:02 +0200

    d

---
+++ Database_server/node/PublicResources/tests/quest.test.js
@@ -0,0 +1,16 @@
+runTests();
+function runTests(){
+    console.log("Running tests for quest_creation...");
+
+    
+
+    console.log("All tests for quest_creation passed");
+}
+
+
+// Assertion function
+function assert(condition, message) {
+    if (!condition) {
+      console.error("Assertion failed:", message);
+    } 
+}
