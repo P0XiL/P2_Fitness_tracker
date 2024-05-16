65fc28dfe1b8035d1cba86b0bcb9bc4f73d26d54
tobia <tlva23@student.aau.dk>
2024-05-16 09:52:43 +0200

    start making

---
+++ Database_server/node/PublicResources/tests/Graph.test.js
@@ -0,0 +1,12 @@
+// Assertion function
+function assert(condition, message) {
+    if (!condition) {
+      console.error("Assertion failed:", message);
+    } 
+}
+
+async function testGraph(){
+    console.log(user_data_x())
+    result = 
+    assert()
+}
