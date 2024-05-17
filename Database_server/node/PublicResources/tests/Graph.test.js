259fb981b3d7088da7ad2978bb11769d6d411073
tobia <tlva23@student.aau.dk>
2024-05-17 13:45:28 +0200

    updating test

--- Database_server/node/PublicResources/tests/Graph.test.js
+++ Database_server/node/PublicResources/tests/Graph.test.js
@@ -19,17 +19,30 @@ function assert(condition, message) {
     } 
 }
 
+async function compare(arr1, arr2){
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 async function testDataX(){
-    const result = ["1/1/2024","11/5/2024","13/5/2024"]
-    assert(user_data_x('test', 'wallsit') === result, "user_data_y output not matching expected output");
+    const result = ["1/1/2024","11/5/2024","13/5/2024"];
+    assert(compare(user_data_x('test','wallsit'), result), "user_data_y output not matching expected output");
 }
 
 async function testDataY(){
     const result = ["0","18","18"]
-    assert(user_data_y('test', 'wallsit') === result, "user_data_x output not matching expected output");
+    assert(compare(user_data_y('test', 'wallsit'), result), "user_data_x output not matching expected output");
 }
   
 async function testIndividual(){
     const result = {armcircles: 25, crunches: 20, cycling: 10, run: 40, situps: 26, wallsit: 36};
-    assert(individual_type("test", "statsTextUser") === result, "Individual output is not matching the expected output");
+    assert(compare(individual_type("test", "statsTextUser"), result), "Individual output is not matching the expected output");
 }
